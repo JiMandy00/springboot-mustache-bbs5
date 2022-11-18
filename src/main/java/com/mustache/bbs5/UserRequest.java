e9e5afe79b945eff5142e909f61daf762e38051c
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-18 13:00:49 +0900

    22 11 18 - UserRequest에 toEntity() 추가

--- src/main/java/com/mustache/bbs5/UserRequest.java
+++ src/main/java/com/mustache/bbs5/UserRequest.java
@@ -1,5 +1,6 @@
 package com.mustache.bbs5;
 
+import com.mustache.bbs5.entity.User;
 import lombok.*;
 
 @Builder
@@ -9,4 +10,11 @@ import lombok.*;
 public class UserRequest {
     private String username;
     private String password;
+
+    public User toEntity() {
+        return User.builder() // builder() : 필요한 값만 가져올 수 있음
+                .username(this.username)
+                .password(this.password)
+                .build(); // 닫아주기
+    }
 }
