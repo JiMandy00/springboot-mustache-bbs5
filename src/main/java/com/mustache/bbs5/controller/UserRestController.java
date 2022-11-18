8ebb13e7efb2fb1dcea17d50a99654f727cb7838
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-18 13:01:53 +0900

    22 11 18 - UserRestController에 postMapping추가

--- src/main/java/com/mustache/bbs5/controller/UserRestController.java
+++ src/main/java/com/mustache/bbs5/controller/UserRestController.java
@@ -1,19 +1,18 @@
 package com.mustache.bbs5.controller;
 
+import com.mustache.bbs5.UserRequest;
 import com.mustache.bbs5.UserResponse;
 import com.mustache.bbs5.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/users")
-public class UserController {
+public class UserRestController {
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserRestController(UserService userService) {
         this.userService = userService;
     }
 
@@ -21,5 +20,10 @@ public class UserController {
     // ResponseEntity class : Http request, response에 해당하는 http header, body를 포함
     public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
         return ResponseEntity.ok().body(userService.getUser(id));
+    }
+
+    @PostMapping(("/post"))
+    public ResponseEntity<UserResponse> addUser(@RequestBody UserRequest dto) {
+        return ResponseEntity.ok().body(userService.addUser(dto));
     }
 }
